c8d4bfbe23554be86af17f690740b442c9ea2baf
Artur Bauer <bauerart1@gmail.com>
2020-11-17 16:49:05 +0100

    Add lab6 solution 1/2

---
+++ lab_6/Lab6/Lab6/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merry xmas!");
+            ChristmasTree xmasTree = new ChristmasTree();
+            xmasTree.Add(Color.blue, Shape.ball);
+            Console.WriteLine("Bauble color: {0}", xmasTree[0]);
+            Console.WriteLine("Baubles in color ({0}): {1}", Color.blue, xmasTree[Color.blue]);
+            xmasTree[0] = Color.yellow;
+            Console.WriteLine("Color changed");
+            Console.WriteLine("Bauble color: {0}", xmasTree[0]);
+
+            ChristmasTreeA xmasTreeA = new ChristmasTreeA();
+            xmasTreeA.Add(Color.blue, Shape.ball);
+            Console.WriteLine("ChristmasTreeA");
+            Console.WriteLine("Bauble shape: {0}", xmasTreeA[0]);
+            Console.WriteLine("Bauble shape: {0}", ((ChristmasTree) xmasTreeA)[0]);
+
+            ChristmasTreeB xmasTreeB = new ChristmasTreeB();
+            xmasTreeB.Add(Color.blue, Shape.ball);
+            Console.WriteLine("ChristmasTreeB");
+            Console.WriteLine("Bauble: {0}", xmasTreeB[0]);
+            Console.WriteLine("Bauble: {0}", ((ChristmasTreeA) xmasTreeB)[0]);
+        }
+    }
+
+    class Tree
+    {
+        string name;
+        int age;
+    }
+    class Fir : Tree
+    {
+        protected class Bauble
+        {
+            public Color color { get; set; }
+            public Shape shape { get; protected set; }
+            
+            public Bauble(Color c, Shape s)
+            {
+                color = c;
+                shape = s;
+            }
+        }
+
+    }
+    enum Color
+    {
+        blue,
+        green,
+        yellow
+    }
+    enum Shape
+    {
+        star,
+        ball
+    }
+
+    class ChristmasTree  : Fir
+    {
+        protected List<Bauble> baubles;
+        
+        public ChristmasTree()
+        {
+            baubles = new List<Bauble>();
+        }
+
+        public void Add(Color color, Shape shape)
+        {
+            Bauble bauble = new Bauble(color, shape);
+            baubles.Add(bauble);
+        }
+        public void Remove(int index)
+        {
+            baubles.RemoveAt(index);
+        }
+
+        public Color this[int index] {
+            get => baubles[index].color;
+            set => baubles[index].color = value;
+        }
+        public int this[Color color] {
+            get => baubles.FindAll(bouble => bouble.color == color).Count;
+        }
+    }
+
+    class ChristmasTreeA: ChristmasTree
+    {
+        public Shape this[int index]
+        {
+            get => baubles[index].shape;
+        }
+
+        public Color BaubleColor(int index)
+        {
+            return base[index];
+        }
+    }
+
+    class ChristmasTreeB : ChristmasTreeA {
+        public new string this[int index]
+        {
+            get
+            {
+                Bauble b = baubles[index];
+                return b.color.ToString() + "_" + b.shape.ToString();
+            }
+        }
+    
+    }
+
+    sealed class ChristmasTreeC: ChristmasTreeB
+    {
+
+    }
+
+    abstract class Home
+    {
+         protected abstract int Price();
+    }
+}
